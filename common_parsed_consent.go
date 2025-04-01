220cb53da1115406f2be84f3f3c1a7abf96131c6
Inna Yaretsky <>
2025-04-01 11:55:53 +0300

    renamed interface

--- common_parsed_consent.go
+++ common_parsed_consent.go
@@ -1,7 +1,7 @@
 package iabconsent
 
-// ParsedConsentInterface provides a common type for both V1 and V2 parsed consent structures.
-type ParsedConsentInterface interface {
+// ParsedConsentData provides a common type for both V1 and V2 parsed consent structures.
+type ParsedConsentData interface {
 	CheckIfPurposesNotAllowed() bool
 	VendorAllowed(vendor int) bool
 	SuitableToProcess(ps []int, vendor int) bool
